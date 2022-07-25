e5b2a850f6bfacc332d1c6054466b93b6e048bb9
Astral <astralhope@gmail.com>
2022-07-25 10:16:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+cp cloud/banner_r86s package/base-files/files/etc/banner
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrtx86/R86S-XG-F/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.19.1/192.168.39.1/g' package/base-files/files/bin/config_generate
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
